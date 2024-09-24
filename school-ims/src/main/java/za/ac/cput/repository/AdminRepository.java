a64b0bcac60228e0b835f3591e58313687616820
Thabo <justicethabo21@gmail.com>
2024-09-25 01:48:11 +0200

    Admin Added

--- school-ims/src/main/java/za/ac/cput/repository/AdminRepository.java
+++ school-ims/src/main/java/za/ac/cput/repository/AdminRepository.java
@@ -1,4 +1,12 @@
 package za.ac.cput.repository;
 
-public class AdminRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.Admin;
+
+import java.util.Optional;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByEmail(String email);
 }
---
+++ school-ims/src/main/java/za/ac/cput/util/AdminHelper.java
@@ -0,0 +1,9 @@
+package za.ac.cput.util;
+
+public class AdminHelper {
+
+    public static boolean isNullOrEmpty(String value) {
+        return value == null || value.trim().isEmpty();
+    }
+
+}
