d38d2e8c7351b8c6a7b958f3cc4b9c31c6dd311c
Thabo <justicethabo21@gmail.com>
2024-09-28 19:51:22 +0200

    Added controller for requests

--- school-ims/src/main/java/za/ac/cput/service/RequestService.java
+++ school-ims/src/main/java/za/ac/cput/service/RequestService.java
@@ -19,6 +19,10 @@ public class RequestService implements IService<Request, Long> {
 
     @Override
     public Request create(Request request) {
+        // Ensure the product is set before saving
+        if (request.getProduct() == null) {
+            throw new IllegalArgumentException("Product must not be null");
+        }
         return requestRepository.save(request);
     }
 
@@ -30,21 +34,19 @@ public class RequestService implements IService<Request, Long> {
 
     @Override
     public Request update(Request request) {
-        if (requestRepository.existsById(request.getRequestId())) {
-            return requestRepository.save(request);
-        } else {
+        if (!requestRepository.existsById(request.getRequestId())) {
             throw new IllegalStateException("Request with Id " + request.getRequestId() + " does not exist");
         }
+        return requestRepository.save(request);
     }
 
     @Override
     public boolean delete(Long requestId) {
-        if (requestRepository.existsById(requestId)) {
-            requestRepository.deleteById(requestId);
-            return true;
-        } else {
+        if (!requestRepository.existsById(requestId)) {
             throw new IllegalStateException("Request with Id " + requestId + " does not exist");
         }
+        requestRepository.deleteById(requestId);
+        return true;
     }
 
     @Override
@@ -60,6 +62,7 @@ public class RequestService implements IService<Request, Long> {
         return requestRepository.findByProduct_ProductId(productId);
     }
 
+    // Uncomment if you need to delete requests by product ID
 //    public void deleteRequestsByProductId(Long productId) {
 //        requestRepository.deleteByProduct_ProductId(productId);
 //    }
